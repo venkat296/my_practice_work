e362be8aeffbe58eb4fd27b0cf7e0114b6f32e7d
venkatadurgarao <venkatadurgaraoy@gmail.com>
2025-02-06 20:54:01 +0530

    variable

---
+++ 06-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRIUTS=("APPLE" "KIWI" "ORANGE")  #array
+
+echo "firast friut is :: ${FRIUTS[0]}"
+
+echo "second friut is:: ${FRIUTS[1]}"
+
+echo "second friut is:: ${FRIUTS[2]}"
+
+echo "All friuts is:: ${FRIUTS[@]}"
