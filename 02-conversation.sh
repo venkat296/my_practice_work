d93d670ccb5e266c27680ef5bbb1d719f7a65fa0
venkatadurgarao <venkatadurgaraoy@gmail.com>
2025-02-06 19:27:07 +0530

    conversion

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh:: Hi Suresh How are you?"
+echo "Suresh:: hI Ramesh I am doing well. How are you doing"
+echo "Ramesh:: I am also good Suresh. What are doing"
+echo "Suresh"" I have started learing Join devops"
