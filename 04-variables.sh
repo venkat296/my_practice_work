210d5906ba48e57b32c07e44727cc19f97849762
venkatadurgarao <venkatadurgaraoy@gmail.com>
2025-02-06 19:58:19 +0530

    variable

---
+++ 04-variables.sh
@@ -0,0 +1,6 @@
+PERSON1=$1
+PERSON2=$2
+echo "${PERSON1}:: Hi ${PERSON2} How are you?"
+echo "${PERSON2}:: hI ${PERSON1} I am doing well. How are you doing"
+echo "${PERSON1}:: I am also good ${PERSON2}. What are doing"
+echo "${PERSON2}:: I have started learing Join devops"
