ef664282dbb709617b324735d5f830842b28088b
venkatadurgarao <venkatadurgaraoy@gmail.com>
2025-02-06 19:56:29 +0530

    variable

---
+++ o3-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Rajesh
+echo "${PERSON1}:: Hi ${PERSON2} How are you?"
+echo "${PERSON2}:: hI ${PERSON1} I am doing well. How are you doing"
+echo "${PERSON1}:: I am also good ${PERSON2}. What are doing"
+echo "${PERSON2}:: I have started learing Join devops"
