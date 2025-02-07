885bf3605fc20edac1662751b5cd4aa1dbae9632
venkatadurgarao <venkatadurgaraoy@gmail.com>
2025-02-07 20:22:33 +0530

    variable

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,8 +2,9 @@
 
 NUMBER=$1
 
-if [ $NUMBER -gt 20 ] then
-echo "given $NUMBER is less then 20"
+if [ $NUMBER -gt 20 ] 
+then
+    echo "given $NUMBER is less then 20"
 else
-echo "given number is greter then 20"
+    echo "given number is greter then 20"
 fi
